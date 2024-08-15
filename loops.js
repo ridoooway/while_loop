2d4fd8b860e502fc90bc5a2651357a0770a699f6
ridoooway <loops.ridoway@gmail.com>
2024-08-15 23:39:19 +0600

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,12 @@
+const number = [12,33,44,33];
+console.log(number);
+
+for (const num of number){
+    console.log(num);
+}
+
+const fruits = ["banana", "apple", "jackfruit"];
+for (const wow of fruits){
+    console.log(wow);
+    console.log('i want to eat');
+}
---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 4;
+if(number<10){
+    console.log("choto number");
+}
+console.log("something new is cooking");
+
+let num = 0; //loop var   
+while(num < 4) 
+{
+    // console.log('loooping');
+    num = num+1;
+    console.log(num);
+}
